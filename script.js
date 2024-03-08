c28806dd015c3d923eb0ef4bc141d2bbc211bccc
Nazar <116907517+LokiPoki02@users.noreply.github.com>
2024-03-08 13:06:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
     apiKey: "AIzaSyA4EPFB2SCUSUVc9RkDDUXIlcWhIhwCJxM",
-  authDomain: "esp32db-1c7ed.firebaseapp.com",
-  databaseURL: "https://esp32db-1c7ed-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "esp32db-1c7ed",
-  storageBucket: "esp32db-1c7ed.appspot.com",
-  messagingSenderId: "111577244132",
-  appId: "1:111577244132:web:b1922e549685d262c727f1"
+    authDomain: "esp32db-1c7ed.firebaseapp.com",
+    databaseURL: "https://esp32db-1c7ed-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "esp32db-1c7ed",
+    storageBucket: "esp32db-1c7ed.appspot.com",
+    messagingSenderId: "111577244132",
+    appId: "1:111577244132:web:b1922e549685d262c727f1"
 };
 
 firebase.initializeApp(firebaseConfig);
